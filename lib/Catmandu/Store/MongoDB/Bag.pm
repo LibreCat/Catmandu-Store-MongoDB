ceb078fa8fa66d212c3b3d4d8ae6d932e8272a3e
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2019-06-18 09:54:47 +0200

    0.0803

--- lib/Catmandu/Store/MongoDB/Bag.pm
+++ lib/Catmandu/Store/MongoDB/Bag.pm
@@ -2,7 +2,7 @@ package Catmandu::Store::MongoDB::Bag;
 
 use Catmandu::Sane;
 
-our $VERSION = '0.0802';
+our $VERSION = '0.0803';
 
 use Catmandu::Util qw(:is);
 use Catmandu::Store::MongoDB::Searcher;
